3508fa3453ad624c666bbef33418f30e59cf1b3c
Elijah Frederickson <elijah.frederickson@gmail.com>
2015-01-16 13:00:29 -0900

    Add "compatibility" layer for existing tweaks

---
+++ ReachAppIntegrator.h
@@ -0,0 +1,9 @@
+#import <RAWidget.h>
+#import <dlfcn.h>
+#import <objc/runtime.h>
+
+#define CHECK_FOR_REACHAPP \
+if ([NSFileManager.defaultManager fileExistsAtPath:@"/Library/MobileSubstrate/DynamicLibraries/ReachApp.dylib"]) \
+	dlopen("/Library/MobileSubstrate/DynamicLibraries/ReachApp.dylib", RTLD_NOW | RTLD_GLOBAL); \
+
+#define IF_REACHAPP if (objc_getClass("RAWidget"))
